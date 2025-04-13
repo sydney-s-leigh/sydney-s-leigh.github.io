5299c2aa404d426c8c16ce6ef0ff31de63e2410c
Sydney Leigh <sydney.s.leigh@gmail.com>
2025-04-13 21:45:46 +0000

    We should now be ignoring _site.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744573047',resources: [
+    cacheName: 'chirpy-1744573560',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
