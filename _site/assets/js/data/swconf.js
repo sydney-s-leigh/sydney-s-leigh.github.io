cb64c5aa5f948af61ee91382520b419ba94ab01c
Sydney Leigh <sydney.s.leigh@gmail.com>
2025-04-13 21:54:32 +0000

    We should now be ignoring _site, and _config.yaml has been modified to include time and data.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744573560',resources: [
+    cacheName: 'chirpy-1744581127',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
