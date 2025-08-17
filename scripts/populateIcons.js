dd99edee4d793f7969446e4af961d38e610e4f5d
Sydney Leigh <sydney.s.leigh@gmail.com>
2025-08-17 02:08:42 +0000

    I added a javascript that I intend to autopopulate the gridbox with icons when it is finished. I also fixed my apache server on my docker container so that it points to my repository as the webroot so I don't have to absolute code URLs.

---
+++ scripts/populateIcons.js
@@ -0,0 +1,9 @@
+function populateGridBox(){
+    // alert("Hello World!");
+    document.getElementById("gridBoxLeft").innerHTML = "Y";
+
+    //Grab icons from /images/svgs
+
+    //Create an image for every other box in gridBoxLeft based on number of grid-template-columns/grid-template-rows
+    //that is properly resized
+}
