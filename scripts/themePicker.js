d1364c01b660984913cec81a517db837ff9d28d5
Sydney Leigh <sydney.s.leigh@gmail.com>
2025-08-27 14:10:28 +0000

    Theme Picker script working correctly now and saving the theme to browser session.

--- scripts/themePicker.js
+++ scripts/themePicker.js
@@ -1,6 +1,8 @@
 // Get a reference to the HTML element and the toggle button
 const htmlElement = document.documentElement;
-const themeToggle = document.getElementById("theme-toggle");
+const themeLight = document.getElementById("light");
+const themeDark = document.getElementById("dark");
+const themeVGAGreen = document.getElementById("VGAGreen");
 
 // Get the user's saved theme from localStorage, or default to system preference
 const savedTheme = localStorage.getItem("theme");
@@ -11,12 +13,26 @@ const initialTheme = savedTheme || (prefersDark ? "dark" : "light");
 htmlElement.setAttribute("data-theme", initialTheme);
 
 // Add a click event listener to the button
-themeToggle.addEventListener("click", () => {
-  let currentTheme = htmlElement.getAttribute("data-theme");
-  let newTheme = currentTheme === "dark" ? "light" : "dark";
+themeLight.addEventListener("click", () => {
+  // let currentTheme = htmlElement.getAttribute("data-theme");
+  // let newTheme = currentTheme === "dark" ? "light" : "dark";
+
+  const newTheme = "light";
+  // Set the theme and save the new theme to localStorage
   htmlElement.setAttribute("data-theme", newTheme);
-
-  // Save the new theme to localStorage
   localStorage.setItem("theme", newTheme);
+});
 
+themeDark.addEventListener("click", () => {
+  const newTheme = "dark";
+  // Set the theme and save the new theme to localStorage
+  htmlElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+});
+
+themeVGAGreen.addEventListener("click", () => {
+  const newTheme = "VGAGreen";
+  // Set the theme and save the new theme to localStorage
+  htmlElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
 });
